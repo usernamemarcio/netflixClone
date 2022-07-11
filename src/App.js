ba2534423e309110d15e0964fa2d9c26e1177364
Marcio Santos <mpereiradossantos242@gmail.com>
2022-07-11 10:26:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import tmdb from './tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
-function App() {
+export default () => {
+
+  const [movieList, SetMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // pegando a lista total
+      let list = await tmdb.getHomeList();
+      SetMovieList(list);
+
+      // pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
 
-export default App;
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Desenvolvido por <strong>Marcio Santos</strong>.<br/>
+        Direitos de imagem para Netflix.<br/>
+        Dados pegos do site Themoviedb.org<br/>
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+    </div>
+  )
+}
